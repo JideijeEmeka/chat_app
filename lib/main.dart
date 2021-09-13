8b0c227bcc86c8e044037167f2d10277d95f20c2
Jideije Emeka <jideije.emeka@gmail.com>
2021-09-13 16:25:27 -0700

    created auth service file 4 handling signin/up/out

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:chat_app/views/signin.dart';
 import 'package:chat_app/views/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
